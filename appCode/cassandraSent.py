303acf14abc9ca519f043f7d62eda4b280c1b84a
Ulyses <ulyses.rico.2012@gmail.com>
2020-11-05 09:50:25 -0600

    test en base de datos de prueba...

--- appCode/cassandraSent.py
+++ appCode/cassandraSent.py
@@ -4,7 +4,7 @@ from cassandra.auth import PlainTextAuthProvider
 from cassandra.query import SimpleStatement
 import os
 
-
+keyspace='test'
               
 def cassandraBDProcess(json_sentencia):
      
@@ -16,7 +16,7 @@ def cassandraBDProcess(json_sentencia):
         'secure_connect_bundle':objCC.cc_secureBundle
     }
     
-    auth_provider = PlainTextAuthProvider(objCC.cc_user,objCC.cc_pwd)
+    auth_provider = PlainTextAuthProvider(objCC.cc_user_test,objCC.cc_pwd_test)
     cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
     session = cluster.connect()
     session.default_timeout=70
@@ -24,7 +24,7 @@ def cassandraBDProcess(json_sentencia):
     pdfname=json_sentencia['pdfname']
     #Check wheter or not the record exists, check by numberFile and date
     #Date in cassandra 2020-09-10T00:00:00.000+0000
-    querySt="select id from thesis.tbcourtdecisiontfjfa where pdfname='"+str(pdfname)+"'  ALLOW FILTERING"
+    querySt="select id from "+keyspace+".tbcourtdecisiontfjfa where pdfname='"+str(pdfname)+"'  ALLOW FILTERING"
                 
     future = session.execute_async(querySt)
     row=future.result()
@@ -35,7 +35,7 @@ def cassandraBDProcess(json_sentencia):
     else:        
         #Insert Data as JSON
         jsonS=json.dumps(json_sentencia,ensure_ascii=True)           
-        insertSt="INSERT INTO thesis.tbcourtdecisiontfjfa JSON '"+jsonS+"';" 
+        insertSt="INSERT INTO "+keyspace+".tbcourtdecisiontfjfa JSON '"+jsonS+"';" 
         future = session.execute_async(insertSt)
         future.result()  
         sent_added=True
@@ -52,13 +52,13 @@ def updatePage(strquery,page):
         'secure_connect_bundle':objCC.cc_secureBundle
     }
     
-    auth_provider = PlainTextAuthProvider(objCC.cc_user,objCC.cc_pwd)
+    auth_provider = PlainTextAuthProvider(objCC.cc_user_test,objCC.cc_pwd_test)
     cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
     session = cluster.connect()
     session.default_timeout=70
     row=''
     page=str(page)
-    querySt="update thesis.cjf_control set page="+page+" where query='"+strquery+"' and id_control=2;"          
+    querySt="update "+keyspace+".cjf_control set page="+page+" where query='"+strquery+"' and id_control=2;"          
     future = session.execute_async(querySt)
     row=future.result()
                          
@@ -72,13 +72,13 @@ def getPageAndTopic():
         'secure_connect_bundle':objCC.cc_secureBundle
     }
     
-    auth_provider = PlainTextAuthProvider(objCC.cc_user,objCC.cc_pwd)
+    auth_provider = PlainTextAuthProvider(objCC.cc_user_test,objCC.cc_pwd_test)
     cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
     session = cluster.connect()
     session.default_timeout=70
     row=''
     #select page from  thesis.cjf_control where id_control=1 and query='Primer circuito'
-    querySt="select query,page from thesis.cjf_control where id_control=2  ALLOW FILTERING"
+    querySt="select query,page from "+keyspace+".cjf_control where id_control=2  ALLOW FILTERING"
                 
     future = session.execute_async(querySt)
     row=future.result()
@@ -98,9 +98,9 @@ def getPageAndTopic():
    
 class CassandraConnection():
     cc_user='quartadmin'
-    cc_keyspace='thesis'
     cc_pwd='P@ssw0rd33'
-    cc_databaseID='9de16523-0e36-4ff0-b388-44e8d0b1581f'
-    cc_secureBundle='/app/appCode/secure-connect-dbquart.zip'
+    cc_user_test='test'
+    cc_pwd_test='testquart'
+    cc_secureBundle='/app/appCode/secure-connect-dbtest.zip'
         
 
