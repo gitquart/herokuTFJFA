0a655d8efc463b85ccbbd7aacb82444686128817
Ulyses <ulyses.rico.2012@gmail.com>
2020-12-09 21:45:10 -0600

    ...

--- appCodeTFJFA/main.py
+++ appCodeTFJFA/main.py
@@ -64,7 +64,7 @@ if status==200:
         #Update the info in file
         print('Page already done:...',str(startPage))  
         currentPage=int(browser.find_elements_by_css_selector('#dtRresul_paginator_top > span.ui-paginator-pages > span.ui-paginator-page.ui-state-default.ui-corner-all.ui-state-active')[0].text) 
-        bd.updatePage(topic,currentPage+1)
+        bd.updatePage(currentPage+1)
         startPage=currentPage+1
         #Change the page with next
         btnNext=browser.find_elements_by_xpath("//*[@id='dtRresul_paginator_top']/span[4]")[0].click()
