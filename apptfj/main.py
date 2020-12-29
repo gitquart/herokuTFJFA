26f73c87cf4417eb681e7fe4e497f569f7c482e1
Ulyses <ulyses.rico.2012@gmail.com>
2020-12-29 16:09:23 -0600

    ...

--- apptfj/main.py
+++ apptfj/main.py
@@ -83,14 +83,17 @@ if status==200:
     #Mechanism to change to current page
     if startPage>1:
         print('More than 1 page, checking if next btn is enabled...')
-        for x in range(1,startPage):
-            btnNext=tool.devuelveElemento("//*[@id='dtRresul_paginator_top']/span[4]",browser)
-            btnNext.click()
-            
-            """
+        btnNextSelector=browser.find_elements_by_css_selector('#dtRresul_paginator_top > span.ui-paginator-next.ui-state-default.ui-corner-all.ui-state-disabled')[0]
+        nextCount=len(btnNextSelector)
+        print('Paginator disabled count:',str(nextCount))
+        if nextCount>0:
             print('Btn next is NOT enabled, preparing next query...')
             tool.prepareNextQuery(strdates)
-            """
+        else:    
+            for x in range(1,startPage):
+                btnNext=tool.devuelveElemento("//*[@id='dtRresul_paginator_top']/span[4]",browser)
+                btnNext.click()
+            
     print('Start reading the page...')
     #Control the page
     #Page identention
