fea1956156f29ad39adfcfdf76e7b1ab5a5ae801
Ulyses <ulyses.rico.2012@gmail.com>
2021-01-01 21:56:12 -0600

    ...

--- apptfj/main.py
+++ apptfj/main.py
@@ -85,6 +85,7 @@ if status==200:
         print('More than 1 page, checking if next btn is enabled...')
         btnNextSelector=browser.find_elements_by_css_selector('#dtRresul_paginator_top > span.ui-paginator-next.ui-state-default.ui-corner-all.ui-state-disabled')
         lsCount=len(btnNextSelector)
+        print('Value of btnNext count:',str(lsCount))
         if lsCount>0:
             print('Btn next is NOT enabled, preparing next query...')
             tool.prepareNextQuery(strdates)
